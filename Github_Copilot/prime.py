a340f996818bfa93dc0ed892a17fbdcb7ea454d2
Puppala Sagar <puppala.sagar@ryan.com>
2025-08-13 15:08:17 +0530

    added code written during prompt engineering and github copilot courses

---
+++ Github_Copilot/prime.py
@@ -0,0 +1,23 @@
+# generate code for to get the all the prime numbers in a given range
+
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def get_primes_in_range(start, end):
+    """Get all prime numbers in a given range."""
+    primes = []
+    for num in range(start, end + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+prime_numbers = get_primes_in_range(start, end)
+print(f"Prime numbers between {start} and {end}: {prime_numbers}")
